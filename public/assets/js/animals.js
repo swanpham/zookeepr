3a04a040f6f51e7827709b2b4cfabb091b9b3ec9
Swan Pham <accounting@nailsrus.ca>
2021-05-08 14:12:14 -0400

    update html

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -28,19 +28,20 @@ const getAnimals = (formData = {}) => {
   Object.entries(formData).forEach(([key, value]) => {
     queryUrl += `${key}=${value}&`;
   });
+
   console.log(queryUrl);
-  
+
   fetch(queryUrl)
-  .then(response => {
-    if (!response.ok) {
-      return alert('Error: ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(animalData => {
-    console.log(animalData);
-    printResults(animalData);
-  });  
+    .then(response => {
+      if (!response.ok) {
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(animalData => {
+      console.log(animalData);
+      printResults(animalData);
+    });
 };
 
 const handleGetAnimalsSubmit = event => {
